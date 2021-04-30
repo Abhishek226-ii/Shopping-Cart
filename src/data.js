5ed0deb53b80c9fc6658700a6edf5b9472277c56
Abhishek226-ii <anirudh89368@gmail.com>
2021-04-30 15:34:28 +0530

    First Commit

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const data = {
+    products: [
+        {
+            id: '1',
+            name: 'Poster',
+            price: 2000,
+            Image: 'https://picsum.photos/seed/picsum/200/300',
+        },
+        {
+            id: '2',
+            name: 'LampSet',
+            price: 3000,
+            Image: 'https://picsum.photos/200/300/?blur=2',
+            
+        },
+        {
+            id: '3',
+            name: 'StuffDog',
+            price: 1000,
+            Image: 'https://picsum.photos/id/237/200/300',
+        },
+
+
+    ],
+
+};
+
+export default data;
